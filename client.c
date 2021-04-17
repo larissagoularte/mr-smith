dbd8cc4d46354b358432c126583490d1d53bbcb4
larissagoularte <58703854+larissagoularte@users.noreply.github.com>
2021-04-17 08:37:46 -0700

    changes in server and client

--- client.c
+++ client.c
@@ -1,70 +1,161 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
 #include <arpa/inet.h>
-#define SIZE 1024
-
-void send_file(FILE *fp, int sockfd)
-{
-	char data[SIZE] = {0};
-
-	while(fgets(data, SIZE, fp) != NULL)
-	{
-		if(send(sockfd, data, sizeof(data), 0) == -1)
-		{
-			perror("Error in sending file.\n");
-			exit(1);
-		}
-		bzero(data, SIZE);
-	}
-	return;
-}
-
-
+#include <netinet/in.h>
 int main()
 {
-	char *ip = "127.0.0.1";
-	int port = 8080;
-	int e;
-
-	int sockfd;
-	struct sockaddr_in server_addr;
-	FILE *fp;
-	const char *filename = "teste.txt";
-
-	sockfd = socket(AF_INET, SOCK_STREAM, 0);
-	if(sockfd<0)
+	int fd;
+	char textFileName[2000], audioFileName[2000], videoFileName[2000], imageFileName[2000], file_buffer[2000], c, caufile[70000], aufile[7000000], vfile[1000000];
+	struct sockaddr_in server_address; 
+	
+	//socket file descriptor
+	if((fd = socket(AF_INET, SOCK_DGRAM, 0)) < 0)
 	{
-		perror("Error in socket.\n");
-		exit(1);
+		perror("Error creating socket");
+		exit(EXIT_FAILURE);
 	}
-	printf("Client socket created\n");
-
-	server_addr.sin_family = AF_INET;
-	server_addr.sin_port = port;
-	server_addr.sin_addr.s_addr = inet_addr(ip);
-
-	e = connect(sockfd, (struct sockaddr*)&server_addr, sizeof(server_addr));
-	if(e==-1)
+	
+	memset(&server_address, 0, sizeof(server_address)); 
+	
+	bzero(&server_address, sizeof(server_address)); 
+	
+	//server information
+	server_address.sin_family = AF_INET; 
+	server_address.sin_port = htons(8000); //Random port number, can be changed later
+	server_address.sin_addr.s_addr = INADDR_ANY; 
+	//server_address.sin_addr.s_addr = inet_addr("127.0.0.1"); 
+	
+	int choice = 1; 
+	
+	while(choice != 5)
 	{
-		perror("Error connecting.\n");
-		exit(1);
-	}
-	printf("Connected to server.\n");
-
-	fp = fopen(filename, "r");
-	if(fp==NULL)
-	{
-		perror("Error in reading file.");
-		exit(1);
-	}
-
-	send_file(fp, sockfd);
-	printf("File sent.\n");
-
-	close(sockfd);
-	printf("Disconnected from the server.\n");
-
+		printf("Choose the type of file to send.\n");
+		printf("1 - Text\n");
+		printf("2 - Audio\n");
+		printf("3 - Video\n");
+		printf("4 - Image\n");
+		printf("5 - Exit\n");
+		scanf("%d", &choice); 
+		
+		char num = choice; 
+		
+		sendto(fd, &num, sizeof(num), 0, (struct sockaddr *)&server_address, sizeof(struct sockaddr));
+		
+		switch(choice)
+		{
+			case 1:
+				printf("Enter text file name: \n");
+				scanf("%s",textFileName); 
+				sendto(fd, textFileName, strlen(textFileName), 0, (struct sockaddr *)&server_address, sizeof(struct sockaddr));
+				
+				FILE *fp;
+				fp=fopen(textFileName, "r");
+				
+				if(fp)
+				{
+					printf("Reading file content...\n");
+					fseek(fp,0,SEEK_END);
+					size_t file_size=ftell(fp);
+					fseek(fp, 0, SEEK_SET);
+					if(fread(file_buffer, file_size, 1, fp) <= 0)
+					{
+						printf("Unable to copy file to buffer or file is empty.\n");
+						exit(1);
+					}
+				}
+				else
+				{
+					printf("Error opening file.\n");
+					exit(0);
+				}
+				
+				if(sendto(fd, file_buffer, strlen(file_buffer), 0, (struct sockaddr *)&server_address, sizeof(struct sockaddr)) < 0)
+				{
+					printf("File not sent.\n");
+				}
+				else
+				{
+					printf("File sent.\n");
+				}
+				fclose(fp);
+				break;
+				
+			case 2:
+				printf("Enter audio file name: \n");
+				scanf("%s", audioFileName);
+				sendto(fd, audioFileName, strlen(audioFileName), 0, (struct sockaddr *)&server_address, sizeof(struct sockaddr));
+				
+				FILE *afp; 
+				afp = fopen(audioFileName, "r"); 
+				fseek(afp, 0, SEEK_END); 
+				size_t afsize=ftell(afp);
+				fseek(afp, 0, SEEK_SET); 
+					
+				if(afp)
+				{
+					if(fread(aufile, afsize, 1, afp) <= 0)
+					{
+						printf("Unable to copy file to buffer or file is empty.\n");
+						exit(1);
+					}
+				}
+				else
+				{
+					printf("Could not read audio file.\n");
+					exit(0);
+				}
+				
+				if(sendto(fd, aufile, afsize, 0, (struct sockaddr *)&server_address, sizeof(struct sockaddr)) < 0)
+				{
+					printf("File not sent.\n");
+				}
+				else
+				{
+					printf("File sent.\n"); 
+				}
+				
+				fclose(afp); 
+				break;
+		
+			case 3: 
+				printf("Enter video file name: \n"); 
+				scanf("%s", videoFileName);
+				sendto(fd, videoFileName, strlen(videoFileName), 0, (struct sockaddr *)&server_address, sizeof(struct sockaddr)); 
+				
+				FILE *vfp; 
+				vfp = fopen(videoFileName, "r");
+				
+				fseek(vfp, 0, SEEK_END); 
+				size_t vfsize = ftell(vfp);
+				fseek(vfp, 0, SEEK_SET);
+				
+				if(vfp)
+				{
+					if(fread(vfile, 1, vfsize, vfp) <= 0)
+					{
+						printf("Error reading file.\n");
+					}
+				}
+				else
+				{
+					printf("Could not read video file.\n"); 
+					exit(0);
+				}
+				
+				if(sendto(fd, vfile, vfsize, 0, (struct sockaddr *)&server_address, sizeof(struct sockaddr)) < 0)
+				{
+					printf("File not sent.\n"); 
+				}
+				else
+				{
+					printf("File not sent.\n");
+				}
+				break;
+		}			
+	}	
 	return 0;
 }
--- server.c
+++ server.c
@@ -1,81 +1,120 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <arpa/inet.h>
+#include<sys/socket.h>
+#include<arpa/inet.h>
+#include<stdio.h>
+#include<unistd.h>
+#include<fcntl.h>
+#include<sys/types.h>
+#include<string.h>
+#include<stdlib.h>
+#define maxlen 70000
+#define mlen 100000
 
-#define SIZE 1024
-
-void write_file(int sockfd)
-{
-	int n;
-	FILE *fp;
-	char *filename = "received.txt";
-	char buffer[SIZE];
-
-	fp = fopen(filename, "w");
-	if(fp==NULL)
-	{
-		perror("Error creating file.\n");
-		exit(1);
-	}
-
-	while(1)
-	{
-		n = recv(sockfd, buffer, SIZE, 0);
-		if(n<=0)
-		{
-			break;
-			return;
-		}
-		fprintf(fp, "%s", buffer);
-		bzero(buffer,SIZE);
-	}
-}
 int main()
 {
-	char *ip = "127.0.0.1";
-	int port = 8080;
-	int e;
-
-	int sockfd, new_sock;
-	struct sockaddr_in server_addr, new_addr;
-	socklen_t addr_size;
-	char buffer[SIZE];
-
-	sockfd = socket(AF_INET, SOCK_STREAM, 0);
-	if(sockfd < 0)
+	char fileName[100];
+	char fileBuffer[2000], caufile[maxlen];
+	char *vfilep;
+	int aufile[700000], vfile[mlen];
+	int sd, connfd, len;
+	
+	for(int i=0; i<=100;i++)
 	{
-		perror("Error in socket.\n");
-		exit(1);
+		fileName[i] ='\0';
 	}
-	printf("Server socket created.\n");
-
-	server_addr.sin_family = AF_INET;
-	server_addr.sin_port = port;
-	server_addr.sin_addr.s_addr = inet_addr(ip);
-
-	e = bind(sockfd, (struct sockaddr*)&server_addr, sizeof(server_addr));
-	if(e<0)
+	
+	struct sockaddr_in server_address, client_address;
+	
+	sd = socket(AF_INET, SOCK_DGRAM, 0);
+	
+	if(sd == -1)
 	{
-		perror("Error in binding\n");
-		exit(1);
+		printf("Error creating server socket.\n");
+		exit(0);
 	}
-	printf("Binding successful.\n");
-
-	e = listen(sockfd, 10);
-	if(e==0)
-	{
-		printf("Listening...\n");
-	} 
 	else
 	{
-		perror("Error in listening. \n");
-		exit(1);
+		printf("Server socket created.\n"); 
 	}
-
-	addr_size = sizeof(new_addr);
-	new_sock = accept(sockfd, (struct sockaddr*)&new_addr, &addr_size);
-
-	write_file(new_sock);
-	printf("Data written in the file.\n");
+	
+	bzero(&server_address, sizeof(server_address));
+	
+	server_address.sin_family = AF_INET;
+	server_address.sin_addr.s_addr = INADDR_ANY;
+	server_address.sin_port = htons(8000);
+	
+	memset(&(server_address.sin_zero), '\0', 8);
+	
+	if(bind(sd, (struct sockaddr *)&server_address, sizeof(server_address)) != 0)
+	{
+		printf("Error in binding.\n");
+	}
+	else
+	{
+		printf("Binded.\n");
+	}
+	
+	len = sizeof(client_address);
+	
+	int choice = 1; 
+	
+	while(1)
+	{
+		char num;
+		recvfrom(sd, &num, sizeof(num), 0, (struct sockaddr *)&client_address, &len);
+		choice = num;
+		
+		switch(choice)
+		{
+			case 1: 
+				recvfrom(sd, fileName, 1024, 0, (struct sockaddr *)&client_address, &len);
+				printf("File received: %s\n", fileName);
+				
+				FILE *fp;
+				recvfrom(sd, fileBuffer, 1024, 0, (struct sockaddr *)&client_address, &len);
+				
+				int fsize = strlen(fileBuffer);
+				fp = fopen(fileName, "w");
+				
+				if(fp)
+				{
+					fwrite(fileBuffer, fsize, 1, fp);
+					printf("File received.\n"); 
+				}
+				else 
+				{
+					printf("Error creating output file.\n");
+				}
+				
+				memset(fileName, '\0', sizeof(fileName));
+				fclose(fp);
+				break;
+				
+				case 2:
+					recvfrom(sd, fileName, 1024, 0, (struct sockaddr *)&client_address, &len); 
+					printf("File received: %s\n", fileName);
+					
+					FILE *afp; 
+					int numbytes;
+					
+					afp = fopen(fileName, "w");
+					size_t afsize;
+					afsize = recvfrom(sd, aufile, 700000, 0, (struct sockaddr *)&client_address, &len);
+					
+					if(afp)
+					{
+						fwrite(aufile, afsize, 1, afp);
+						printf("File received successfully.\n");
+					}
+					else
+					{
+						printf("Cannot open output file.\n");
+					}
+					
+					memset(fileName, '\0', sizeof(fileName));
+					fclose(afp);
+					
+					break;
+		}
+	}
+	return 0;
 }
